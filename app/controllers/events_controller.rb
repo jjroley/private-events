29d0dd6f84f6e92f278599a9d33f96ed59003386
Jonathan Roley <jonathan.roley@gmail.com>
2022-11-29 16:02:10 -0500

    idk

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,11 +15,24 @@ class EventsController < ApplicationController
 
   def create
     @event = current_user.created_events.build(event_params)
-
     if @event.save
+      @event.attendees << current_user
       redirect_to @event, notice: 'Event successfully created'
     else
       render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "Successfully updated"
+    else
+      render :edit
     end
   end
 
