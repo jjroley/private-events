a17c06a9145d227ec6c163f68db21eefff63a74b
Jonathan Roley <jonathan.roley@gmail.com>
2022-11-29 13:59:56 -0500

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
+
+  get 'users/index'
+  match '/users/:username', to: 'users#show', via: 'get'
+
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
 
