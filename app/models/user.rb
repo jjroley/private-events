ec12515e6ff0da3d188d1fd357157ff9c541ebd8
Jonathan Roley <jonathan.roley@gmail.com>
2022-11-28 20:28:48 -0500

    idk auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if (login = conditions.delete(:login))
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
     end
   end
 
@@ -19,5 +23,8 @@ class User < ApplicationRecord
     @login || self.username || self.email
   end
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false } 
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: true
+
+
 end
