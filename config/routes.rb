a962275fa1ef81ea8507853df92e63613603f470
Jonathan Roley <jonathan.roley@gmail.com>
2022-11-29 11:57:31 -0500

    loooks goood

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
-  
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
 
-  resources :events, only: [:index, :show]
+  resources :events
   
   root to: 'events#index'
 end
